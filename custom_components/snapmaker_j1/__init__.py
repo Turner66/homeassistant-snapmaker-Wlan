bb0168f7b26d79a753053bc143ed690c6f2896be
Tim Winter <101426158+Turner66@users.noreply.github.com>
2026-05-04 19:22:26 +0200

    Update __init__.py

--- custom_components/snapmaker_j1/__init__.py
+++ custom_components/snapmaker_j1/__init__.py
@@ -7,7 +7,6 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PORT, CONF_TIMEOUT
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import SnapmakerJ1Api
 
@@ -18,15 +17,17 @@ PLATFORMS = ["sensor", "button"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Snapmaker J1 from a config entry."""
-    session = async_get_clientsession(hass)
-
+    """Set up Snapmaker J1 from config entry."""
     api = SnapmakerJ1Api(
-        session=session,
         host=entry.data[CONF_HOST],
         port=entry.data.get(CONF_PORT, 8080),
         timeout=entry.data.get(CONF_TIMEOUT, 5),
     )
+
+    ok = await api.connect()
+    if not ok:
+        _LOGGER.error("Could not connect to Snapmaker J1 socket server")
+        return False
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = api
@@ -36,10 +37,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    """Unload entry."""
+    api = hass.data[DOMAIN].pop(entry.entry_id, None)
+    if api:
+        await api.disconnect()
 
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id, None)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
